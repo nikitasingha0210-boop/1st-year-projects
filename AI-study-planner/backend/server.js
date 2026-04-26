135b5d020832683bc88962d6db08f225c10d4fcb
Nikita <nikitasingha0210@gmail.com>
2026-04-26 16:38:59 +0530

    Initialize Express server for study planner API

---
+++ AI-study-planner/backend/server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post("/generate-plan", (req, res) => {
+  const { hours } = req.body;
+
+  const subjects = [
+    "Chemistry",
+    "Mathematics",
+    "Physics",
+    "Computer Application",
+    "English",
+    "Hindi"
+  ];
+
+  const perSubject = (hours / subjects.length).toFixed(1);
+
+  let plan = [];
+  let start = 9;
+
+  subjects.forEach(sub => {
+    let end = start + parseFloat(perSubject);
+
+    plan.push({
+      subject: sub,
+      time: `${start}:00 - ${end.toFixed(1)}:00`
+    });
+
+    start = end;
+  });
+
+  res.json({ plan });
+});
+
+app.listen(5000, () => {
+  console.log("Server running on http://localhost:5000");
+});
